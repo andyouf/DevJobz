c305554751c8d9c0a78e7b6bd194c537fa5b21d5
Andrew Ossi <andrewossi1@gmail.com>
2020-07-18 22:37:52 -0400

    finished module 1

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello, world!!!!!");
+const express = require('express');
+const server = express();
+
+server.listen(3000, () => {
+    console.log('I am listening...');
+});
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.json());
+server.post('/job-search', (req, res) => {
+    res.send({
+        searchData: req.body,
+        status: "PENDING",
+    })
+});
+
