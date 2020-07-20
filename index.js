e453e0e4a217ffcd2bfa99055bf459e200f08165
Andrew Ossi <andrewossi1@gmail.com>
2020-07-19 22:32:05 -0400

    adds live data, adds inspirational quotes

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 // const { PORT = 3000, WEATHER_KEY } = process.env;
+// pulls in library from node_modules that I installed
 const express = require('express');
+// instantiates a new app (web server)
 const server = express();
 const axios = require('axios')
 const cowsay = require('cowsay');
 const Quote = require('inspirational-quotes')
-
+// middleware - runs between a request making it to our server and us responding to it
 const morgan = require('morgan');
+// actually hooks up our morgan middleware to our instance of our web server
 server.use(morgan('dev'));
-
+// Static middleware - serves up files stored in the given folder if requested
 server.use(express.static('public'));
 
 const bodyParser = require('body-parser');
+// body parser - unpack the HTTP request body and put it in a format that's nice to work with. Puts it on the req.body
 server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: false }));
-// server.post('/job-search', (req, res) => {
-//     res.send({
-//         searchData: req.body,
-//         status: "PENDING",
-//     })
-// });
+server.use(bodyParser.urlencoded({ extended: false })); // unpacking requests that came in urlencoded
+
 server.post('/job-search', async (req, res) => {
     try {
         const { description, fulltime } = req.body;
@@ -52,6 +51,7 @@ server.get('/cowspiration', (req, res) => {
 
     res.send({ cow });
 });
+// if someone makes a GET request to our app at this URI: 'hello' runs this code...
 server.get('/hello', (req, res, next) => {
     res.send(`
     <html>
@@ -68,6 +68,7 @@ console.log(cowsay.say({
 }));
 console.log(Quote.getQuote())
 
+// setting up our app to run indefinitely, listening on our given port
 server.listen(3000, () => {
     console.log('I am listening...');
 });
