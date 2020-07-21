72a57f177fcbf9d7f46e6b9e15135c2e2ce47a19
Andrew Ossi <andrewossi1@gmail.com>
2020-07-20 22:30:26 -0400

    adds live data, adds inspirational quotes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
-async function getResults() {
+$('#job-search').on('submit', async function (event) {
+    event.preventDefault();
     try {
         const response = await fetch(`/job-search`, {
             method: "POST",
@@ -12,22 +13,9 @@ async function getResults() {
         });
         const { results } = await response.json();
         console.log("these are the results:", results);
-        return results;
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-getResults();
-
-$('#job-search').on('submit', async function (event) {
-    event.preventDefault();
-    try {
-        const response = await fetch(`/job-search`, getResults());
-        const { results } = await response.json();
         updateResults(results);
     } catch (error) {
-        console.error;
+        console.log(error)
     }
 });
 
@@ -39,19 +27,19 @@ function updateResults(results) {
     root.empty();
     results.forEach(function (results) {
         root.append(renderResults(results));
-        console.log(data);
+
     });
 }
 
 
 function renderResults(results) {
-    const { id, type, company, location, title, description, how_to_apply }
+    const { id, type, company, location, title, description, how_to_apply } = results;
 
     let resultEl =
         $(` <div class = "job-results">
     <div class = "jobs-info>
         <h2> ${results.title}</h2>
-        <p> ${full_time ? full_time : ""}
+       
         <h4> ${results.company}</h4>
         <p> ${results.location}, ${results.type}</p>
         <p>${results.description}</p>
