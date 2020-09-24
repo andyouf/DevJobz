168633548eca7c936b4981c654376b41f4649de0
andyouf <andrewossi1@gmail.com>
2020-09-23 22:55:12 -0400

    test update

--- index.js
+++ index.js
@@ -1,75 +1,69 @@
 // sys update
 // const { PORT = 3000, WEATHER_KEY } = process.env;
-// pulls in library from node_modules that I installed
-const express = require('express');
+// pulls in library from node_modules I installed
+const express = require("express");
 // instantiates a new app (web server)
 const server = express();
-const axios = require('axios')
-const cowsay = require('cowsay');
-const Quote = require('inspirational-quotes')
-// middleware - runs between a request making it to our server and us responding to it
-const morgan = require('morgan');
-// actually hooks up our morgan middleware to our instance of our web server
-server.use(morgan('dev'));
-// Static middleware - serves up files stored in the given folder if requested
-server.use(express.static('public'));
+const axios = require("axios");
+const cowsay = require("cowsay");
+const Quote = require("inspirational-quotes");
+// middleware - runs between a request making it to server and responding to it
+const morgan = require("morgan");
+// actually hooks up morgan middleware to instance of web server
+server.use(morgan("dev"));
+// static middleware - serves up files stored in the given folder if requested
+server.use(express.static("public"));
 
-const bodyParser = require('body-parser');
-// body parser - unpack the HTTP request body and put it in a format that's nice to work with. Puts it on the req.body
+const bodyParser = require("body-parser");
+// body parser - unpack the HTTP request body and put it in a format that's nice to work with ... puts it on the req.body
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false })); // unpacking requests that came in urlencoded
 
-server.post('/job-search', async (req, res) => {
-    try {
-        const { description, fulltime } = req.body;
+server.post("/job-search", async (req, res) => {
+  try {
+    const { description, fulltime } = req.body;
 
-        const URL = `https://jobs.github.com/positions.json?${
-            description ? `description=${description}&` : ''
-            }${
-            fulltime ? 'fulltime=true' : ''
-            }`;
+    const URL = `https://jobs.github.com/positions.json?${
+      description ? `description=${description}&` : ""
+    }${fulltime ? "fulltime=true" : ""}`;
 
-        const { data } = await axios.get(URL);
-        res.send({ results: data });
-
-
-
-
-
-
-    } catch (error) {
-        res.send({ error });
-    }
+    const { data } = await axios.get(URL);
+    res.send({ results: data });
+  } catch (error) {
+    res.send({ error });
+  }
 });
 
-server.get('/cowspiration', (req, res) => {
-    const { text, author } = Quote.getQuote();
+server.get("/cowspiration", (req, res) => {
+  const { text, author } = Quote.getQuote();
 
-    const cow = cowsay.say({
-        text: `${text}\n\n- ${author}`,
-        W: 80,
-    });
+  const cow = cowsay.say({
+    text: `${text}\n\n- ${author}`,
+    W: 80,
+  });
 
-    res.send({ cow });
+  res.send({ cow });
 });
-// if someone makes a GET request to our app at this URI: 'hello' runs this code...
-server.get('/hello', (req, res, next) => {
-    res.send(`
+// if someone makes a GET request to app at this URI: 'hello' runs this code...
+server.get("/hello", (req, res, next) => {
+  res.send(`
     <html>
     <head></head>
     <body>
       <h3>Hello!</h3>
     </body>
     </html>
-    `)
+    `);
 });
 
-console.log(cowsay.say({
-    text: "Bah!"
-}));
-console.log(Quote.getQuote())
+console.log(
+  cowsay.say({
+    text: "Bah!",
+  })
+);
+console.log(Quote.getQuote());
 
-// setting up our app to run indefinitely, listening on our given port
+// setting up app to run indefinitely, listening on given port
 server.listen(3000, () => {
-    console.log('I am listening...');
+  console.log("I am listening...");
 });
--- public/app.js
+++ public/app.js
@@ -1,42 +1,44 @@
-$('#job-search').on('submit', async function (event) {
-    event.preventDefault();
-    try {
-        const response = await fetch(`/job-search`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                description: $("#description").val(),
-                fulltime: $("#fulltime").val()
-            })
-        });
-        const { results } = await response.json();
-        console.log("these are the results:", results);
-        updateResults(results);
-    } catch (error) {
-        console.log(error)
-    }
+$("#job-search").on("submit", async function (event) {
+  event.preventDefault();
+  try {
+    const response = await fetch(`/job-search`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        description: $("#description").val(),
+        fulltime: $("#fulltime").val(),
+      }),
+    });
+    const { results } = await response.json();
+    console.log("these are the results:", results);
+    updateResults(results);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-
-
-
 function updateResults(results) {
-    const root = $('#results');
-    root.empty();
-    results.forEach(function (results) {
-        root.append(renderResults(results));
-
-    });
+  const root = $("#results");
+  root.empty();
+  results.forEach(function (results) {
+    root.append(renderResults(results));
+  });
 }
 
-
 function renderResults(results) {
-    const { id, type, company, location, title, description, how_to_apply } = results;
+  const {
+    id,
+    type,
+    company,
+    location,
+    title,
+    description,
+    how_to_apply,
+  } = results;
 
-    let resultEl =
-        $(` <div class = "job-results">
+  let resultEl = $(` <div class = "job-results">
     <div class = "jobs-info>
         <h2> ${results.title}</h2>
        
@@ -45,15 +47,17 @@ function renderResults(results) {
         <p>${results.description}</p>
         <p>${results.how_to_apply}</p>
     </div>
-    </div>`).data("results", results)
-    return resultEl
+    </div>`).data("results", results);
+  return resultEl;
 }
 
 async function fetchQuote() {
-    const response = await fetch('/cowspiration');
-    const { cow } = await response.json();
+  const response = await fetch("/cowspiration");
+  const { cow } = await response.json();
 
-    $('#results').empty().append($(`<pre class="cow">${cow}</pre>`).addClass('open'))
+  $("#results")
+    .empty()
+    .append($(`<pre class="cow">${cow}</pre>`).addClass("open"));
 }
 
 fetchQuote();
